3d4d644b6accc272945645bbd102440d76e3c714
alexpac27 <alexpac27@gmail.com>
2020-06-14 20:11:19 -0400

    done

--- nyc_api.rb
+++ nyc_api.rb
@@ -12,12 +12,6 @@ require 'net/http'
     response.body
   end
 
-  def program_school
-    programs = JSON.parse(self.get_programs)
-    programs.collect do |program|
-      program["agency"]
-    end
-  end
 
   def program_school
     # we use the JSON library to parse the API response into nicely formatted JSON
